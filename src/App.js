b592cbcaafeb28a463db075957ef7853ac86ff6b
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-18 22:43:09 +0900

    create repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="App">테스티 이에요</div>;
+    return <div className="App">소프트웨어 공학</div>;
 }
 
 export default App;
