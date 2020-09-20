cce7326c49d5a76a66ccfd170977e01345c6064e
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-20 15:44:33 +0900

    Add Router

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar(props) {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Link style={{ marginRight: 20 }} to={'/'}>
+                메인
+            </Link>
+            <Link style={{ marginRight: 20 }} to={'/login'}>
+                로그인
+            </Link>
+            <Link style={{ marginRight: 20 }} to={'/register'}>
+                회원가입
+            </Link>
+        </div>
+    );
+}
+
+export default NavBar;
