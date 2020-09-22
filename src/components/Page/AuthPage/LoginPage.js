bc5d04b30381016eb9bf2ba5b3a2409e209e69cc
KKN <cksdl1313@naver.com>
2020-09-22 17:41:45 +0900

    test

--- src/components/Page/AuthPage/LoginPage.js
+++ src/components/Page/AuthPage/LoginPage.js
@@ -3,7 +3,7 @@ import Input from '@material-ui/core/Input';
 function LoginPage(props) {
     return (
         <div>
-            <h1>로그인 페이지입니다</h1>
+            <h1>로그인 sdfdsfdsfsdfds페이지입니</h1>
         </div>
     );
 }
