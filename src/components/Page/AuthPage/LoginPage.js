e71b7a19ccf69d0ed3b0565e5dcfc19be7cbc396
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-22 17:52:44 +0900

    Test

--- src/components/Page/AuthPage/LoginPage.js
+++ src/components/Page/AuthPage/LoginPage.js
@@ -3,8 +3,7 @@ import Input from '@material-ui/core/Input';
 function LoginPage(props) {
     return (
         <div>
-            <h1>로그인 sdfdsfdsfsdfds페이지입니</h1>
-
+            <h1>로그인 sdfdsfs</h1>
         </div>
     );
 }
