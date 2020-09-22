160ebf4aac23696ede0bfb830ada4c7b2fee4e9c
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-22 22:52:58 +0900

    feat: Add api_manager

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,5 @@
-import React, {createContext, useState} from 'react';
+import React, { createContext, useState } from 'react';
 import api from '../api_manager';
-import AsyncStorage from '@react-native-community/async-storage';
-
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
@@ -31,44 +29,37 @@ const AuthContextProvider = (props) => {
     const fetchProfile = async () => {
         let res = await api.get('v1/me/info/');
 
-        if(!res.ok){
+        if (!res.ok) {
             _setUser(userModel);
             setIsAuthenticated(false);
-            return false
+            return false;
         }
         _setUser(res.data);
-        AsyncStorage.setItem('chatToken', res.data[0].chatToken);
         setIsAuthenticated(true);
         return res.data;
     };
 
     const patchProfile = async (values) => {
         let res = await api.patch('v1/me/info/', values);
-        if(!res.ok){return false}
+        if (!res.ok) {
+            return false;
+        }
         _setUser(res.data);
-        return true
+        return true;
     };
 
-    const state = {user, isAuthenticated};
-    const actions = {fetchProfile, patchProfile, addUserInfo};
+    const state = { user, isAuthenticated };
+    const actions = { fetchProfile, patchProfile, addUserInfo };
 
     return (
-        <Provider value={{...state, ...actions }}>
-            {props.children}
-        </Provider>
-    )
+        <Provider value={{ ...state, ...actions }}>{props.children}</Provider>
+    );
 };
 
-const withAuthContext = ChildComponent => props => (
+const withAuthContext = (ChildComponent) => (props) => (
     <AuthContext.Consumer>
-        {
-            context => <ChildComponent {...props} auth={context}  />
-        }
+        {(context) => <ChildComponent {...props} auth={context} />}
     </AuthContext.Consumer>
 );
 
-export {
-    AuthContext,
-    AuthContextProvider,
-    withAuthContext,
-};
+export { AuthContext, AuthContextProvider, withAuthContext };
