14bcda41aec409d8356eeaaf8c942ed5603440a0
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-29 15:37:10 +0900

    Update url

--- src/api_manager/urls.js
+++ src/api_manager/urls.js
@@ -2,7 +2,7 @@ let url = 'http://localhost:8000/';
 // url = 'http://52.79.223.168/';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://52.79.223.168/';
+    url = 'http://15.164.48.222/';
 }
 
-export const BASE_URL = url+'api/';
+export const BASE_URL = url + 'api/';
