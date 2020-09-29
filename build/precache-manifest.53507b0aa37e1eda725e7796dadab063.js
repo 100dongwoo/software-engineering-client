1e333d532d0b28a7c1534b3799ad692f1a9bf8c8
kimja01 <kimja7045@naver.com>
2020-09-29 17:38:24 +0900

    feat(build): Add build

---
+++ build/precache-manifest.53507b0aa37e1eda725e7796dadab063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024f40a0e787c454591a5aaa6e9155e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f639bd214823aaf82ae",
+    "url": "/static/js/2.ab4fa119.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ab4fa119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9546eff72055bc7e5a97",
+    "url": "/static/js/main.b9aa8ebc.chunk.js"
+  },
+  {
+    "revision": "1b9ab3740759796b4aa7",
+    "url": "/static/js/runtime-main.fb68bb50.js"
+  }
+]);
