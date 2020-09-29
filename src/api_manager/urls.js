3f614b72a52d9f9ef5d002e774393812e2356fc1
kimja01 <kimja7045@naver.com>
2020-09-29 16:56:12 +0900

    feat(url): Add url 8000 port

--- src/api_manager/urls.js
+++ src/api_manager/urls.js
@@ -1,7 +1,7 @@
 let url = 'http://localhost:8000/';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://15.164.48.222/';
+    url = 'http://15.164.48.222:8000/';
 }
 
 export const BASE_URL = url + 'api/';
