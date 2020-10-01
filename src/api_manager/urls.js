0e51db3186d8f18837a0be01596bfc60e886be30
kimja01 <kimja7045@naver.com>
2020-10-01 10:43:49 +0900

    feat(url): Update url

--- src/api_manager/urls.js
+++ src/api_manager/urls.js
@@ -2,7 +2,7 @@ let url = 'http://localhost:8000/';
 url = 'http://15.164.48.222:8000/';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://15.164.48.222:8000/';
+    url = 'http://www.m-ssi.com:8000/';
 }
 
 export const BASE_URL = url + 'api/';
