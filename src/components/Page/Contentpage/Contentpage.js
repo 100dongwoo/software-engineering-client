ed7ecdc82578f278bad1fd66fb304a40ea432062
BAEK DONGWOO <qorehddn123@naver.com>
2020-11-07 18:36:40 +0900

    Update Content

--- src/components/Page/Contentpage/Contentpage.js
+++ src/components/Page/Contentpage/Contentpage.js
@@ -11,12 +11,23 @@ function Contentpage(props) {
         <div className="Container">
             <p className="Banner">L.o.g.o</p>
             <div className="PostBox">
-                <p className="PostName">게시글 제목</p>
-                <p className="PostNameImage"></p>
-                <p className="PostNameDate">작성자 이름</p>
-                <p className="PostNameDate">작성 날짜</p>
-                <p className = "PostBoxLine"> </p>
-                <p className = "PostDate">게시글 내용</p>
+                {/*상단 컨테이너*/}
+                <div style={{ marginLeft: '5%' }}>
+                    <p className="PostName">게시글 제목</p>
+                    <img
+                        className="PostNameImage"
+                        src="https://placeimg.com/40/50/anys"
+                        alt="avatar"
+                    />
+                    <p className="PostNameDate">작성자 이름 : 김재훈</p>
+                    <p className="PostNameDate">작성 날짜 : 2020-02-02</p>
+                </div>
+                {/* 경계선*/}
+                <div className="PostBoxLine"> </div>
+                {/*게시글 내용 컨테이너*/}
+                <div style={{ marginLeft: '5%' }}>
+                    <p className="PostDate">게시글 내용</p>
+                </div>
             </div>
         </div>
     );
