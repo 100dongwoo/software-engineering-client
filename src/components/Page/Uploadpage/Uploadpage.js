2b65b6dfcd6d4c7dd03e9e76893b188276debf9d
BAEK DONGWOO <qorehddn123@naver.com>
2020-11-09 11:43:24 +0900

    Remove upload error

--- src/components/Page/Uploadpage/Uploadpage.js
+++ src/components/Page/Uploadpage/Uploadpage.js
@@ -22,7 +22,7 @@ function Uploadpage(props) {
                     return;
                 }
                 alert('업로드 완료');
-                history('/');
+                history.push('/');
             })
             .catch((err) => {
                 alert(err);
