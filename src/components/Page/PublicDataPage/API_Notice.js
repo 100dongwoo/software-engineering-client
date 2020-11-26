c5485c4c1e9c54ff6a732c48d48d741ec265b625
kimja01 <kimja7045@naver.com>
2020-11-26 19:30:18 +0900

    Add xmlParser

--- src/components/Page/PublicDataPage/API_Notice.js
+++ src/components/Page/PublicDataPage/API_Notice.js
@@ -1,20 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import api from '../../../api_manager';
+import api, { extraApi } from '../../../api_manager';
+
+const apiKey = 'UO7tvHBrpODqQ%2BFLE4u3%2FRWyekRHkB5tnV%2B3OS2FaYJeT8xLTF2d5Qa7xH6y32xBp9BJR5eex%2FOPNb0s0zpfeg%3D%3D';
+const url = 'http://openapi.kised.or.kr/openapi/service/rest/ContentsService/getNoticeList';
+let DOMParser = require('xmldom').DOMParser;
+
 
 function ApiNotice(props) {
     const [posts, setPosts] = useState([]);
-    //http://openapi.kised.or.kr/openapi/service/rest/ContentsService/getNoticeList?serviceKey=UO7tvHBrpODqQ%2BFLE4u3%2FRWyekRHkB5tnV%2B3OS2FaYJeT8xLTF2d5Qa7xH6y32xBp9BJR5eex%2FOPNb0s0zpfeg%3D%3D&numOfRows=10&startPage=1&pageSize=10&pageNo=1
+    const [numOfRows, setNumOfRows] = useState(5); //페이지당 게시물 목록 수
+    const [startPage, setStartPage] = useState(1); //시작페이지 번호
+    const [pageSize, setPageSize] = useState(10); //페이지당 게시물 목록 건수
+    const [pageNumber, setPageNumber] = useState(1); //페이지번호
+    //http://openapi.kised.or.kr/openapi/service/rest/StartupInfoSvc/ContentsService/getNoticeList?serviceKey=UO7tvHBrpODqQ%2BFLE4u3%2FRWyekRHkB5tnV%2B3OS2FaYJeT8xLTF2d5Qa7xH6y32xBp9BJR5eex%2FOPNb0s0zpfeg%3D%3D&numOfRows=5&startPage=1&pageSize=10&pageNo=1
+
     useEffect(() => {
         // props.auth.fetchProfile();
-        fetch(
-            'http://openapi.kised.or.kr/openapi/service/rest/ContentsService/getNoticeList?serviceKey=UO7tvHBrpODqQ%2BFLE4u3%2FRWyekRHkB5tnV%2B3OS2FaYJeT8xLTF2d5Qa7xH6y32xBp9BJR5eex%2FOPNb0s0zpfeg%3D%3D&numOfRows=10&startPage=1&pageSize=10&pageNo=1'
-        )
-            .then((res) => res.json())
-            .then((json) => console.log(json))
+        extraApi.get(
+            `${url}?serviceKey=${apiKey}&numOfRows=${numOfRows}&startPage=${startPage}&pageSize=${pageSize}&pageNo=${pageNumber}`
+        ,{'headers' : {'Access-Control-Allow-Origin' : '*'}})
+            .then((res) => res.text())
+            .then((data) => {
+                console.log('aa')
+                let xmlDoc = new DOMParser().parseFromString(data, 'text/xml');
+                console.log(xmlDoc)
+                // console.log(data)
+            })
             .catch((err) => console.log(err));
     }, []);
 
-    return <div>asd</div>;
+
+
+    return <div>asdf</div>;
 }
 
 export default ApiNotice;
