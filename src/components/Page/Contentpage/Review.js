aace1eb179504aacd6aa9babc625ae9d89c70a6b
BAEK DONGWOO <qorehddn123@naver.com>
2020-11-30 20:05:35 +0900

    리뷰 css 수정

--- src/components/Page/Contentpage/Review.js
+++ src/components/Page/Contentpage/Review.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
 import api from '../../../api_manager';
-import {withAuthContext} from '../../../context/AuthContext';
+import { withAuthContext } from '../../../context/AuthContext';
 import { TextArea } from './Contentpage';
-
+import DeleteIcon from '@material-ui/icons/Delete';
+import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined';
+import Tooltip from '@material-ui/core/Tooltip';
 const Container = styled.div`
     width: 95%;
     height: auto;
     margin-top: 2rem;
-     padding: 1% 1%;
-   
+    padding: 1% 1%;
 `;
 const Avartar = styled.img`
     height: 4rem;
@@ -25,20 +26,22 @@ const ReviewConainer = styled.div`
 `;
 
 function Review(props) {
-    const {review, postId, fetchReviews} = props;
+    const { review, postId, fetchReviews } = props;
     const [isUpdateClicked, setIsUpdateClicked] = useState(false);
     const [reviewContent, setReviewContent] = useState(review.content);
 
     const onUpdateReview = (content) => {
-        api.patch(`v1/posts/${postId}/reviews/${review.id}/`,{content}).then((res) => {
-            if (!res.ok) {
-                alert('리뷰 업데이트에 실패하였습니다.');
-                return;
+        api.patch(`v1/posts/${postId}/reviews/${review.id}/`, { content }).then(
+            (res) => {
+                if (!res.ok) {
+                    alert('리뷰 업데이트에 실패하였습니다.');
+                    return;
+                }
+                setIsUpdateClicked(false);
+                alert('리뷰가 업데이트되었습니다.');
+                fetchReviews();
             }
-            setIsUpdateClicked(false);
-            alert('리뷰가 업데이트되었습니다.');
-            fetchReviews();
-        });
+        );
     };
 
     const onDeleteReview = () => {
@@ -60,42 +63,58 @@ function Review(props) {
                     style={{
                         display: 'flex',
                         alignItems: 'center',
-                        justifyContent: 'space-between'
+                        justifyContent: 'space-between',
                     }}
                 >
-                    <div style={{display: 'flex', alignItems: 'center'}}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
                         <Avartar
-                            src={review.user.image ? review.user.image : "https://placeimg.com/40/50/anys"}
+                            src={
+                                review.user.image
+                                    ? review.user.image
+                                    : 'https://placeimg.com/40/50/anys'
+                            }
                             alt="avartar"
                         />
-                        <p style={{marginLeft: '0.5rem'}}>
+                        <p style={{ marginLeft: '0.5rem' }}>
                             {review.user.nickname}
                             <br />
                             {moment(review.createAt).format('YYYY-MM-DD')}
                         </p>
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        {review?.user?.id === props.auth.user?.id &&
-                        <div>
-                            <button
-                                onClick={()=>{
-                                    if(isUpdateClicked){onUpdateReview(reviewContent)}
-                                    setIsUpdateClicked(true)
-                                }}
-                            >수정</button>
-                            <button style={{marginLeft: 4}} onClick={onDeleteReview}>삭제</button>
-                        </div>}
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        {review?.user?.id === props.auth.user?.id && (
+                            <div>
+                                <Tooltip title="수정">
+                                    <BorderColorOutlinedIcon
+                                        onClick={() => {
+                                            if (isUpdateClicked) {
+                                                onUpdateReview(reviewContent);
+                                            }
+                                            setIsUpdateClicked(true);
+                                        }}
+                                    />
+                                </Tooltip>
+                                <Tooltip title="삭제">
+                                    <DeleteIcon
+                                        style={{ marginLeft: 4 }}
+                                        onClick={onDeleteReview}
+                                    />
+                                </Tooltip>
+                            </div>
+                        )}
                     </div>
                 </div>
-                {isUpdateClicked ?
+                {isUpdateClicked ? (
                     <TextArea
                         onChange={(e) => {
                             setReviewContent(e.target.value);
                         }}
                         value={reviewContent}
                         placeholder="댓글을 작성해주세요"
-                    /> :
-                <p>{reviewContent}</p>}
+                    />
+                ) : (
+                    <p>{reviewContent}</p>
+                )}
             </ReviewConainer>
         </Container>
     );
