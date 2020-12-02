cdbb0f4848a10b1d6e1c2bdacb16a63770ddbc53
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-03 01:35:20 +0900

    URL 변경

--- src/api_manager/urls.js
+++ src/api_manager/urls.js
@@ -1,9 +1,10 @@
 let url = 'http://localhost:8000/';
 // url = 'http://192.168.35.18:8000/';
-url = 'http://www.m-ssi.com:8000/';
+// url = 'http://www.m-ssi.com:8000/';
+url = 'http://www.man-ssi.com:8000/';
 
 if (process.env.NODE_ENV === 'production') {
-    url = 'http://www.m-ssi.com:8000/';
+    url = 'http://www.man-ssi.com:8000/';
 }
 
 export const BASE_URL = url + 'api/';
