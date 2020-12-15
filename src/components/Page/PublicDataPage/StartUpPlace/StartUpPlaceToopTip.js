2465a9c7360ce8adf6b28c897143d97d7f10ecb9
BAEK DONGWOO <qorehddn123@naver.com>
2020-12-16 04:14:06 +0900

    Update push

--- src/components/Page/PublicDataPage/StartUpPlace/StartUpPlaceToopTip.js
+++ src/components/Page/PublicDataPage/StartUpPlace/StartUpPlaceToopTip.js
@@ -6,12 +6,20 @@ import styled from 'styled-components';
 
 function StartUpPlaceToopTip({ place }) {
     const [posisition, setPosition] = useState('');
+    const [posisition1, setPosition1] = useState('');
     const [isTooltipActive, setIsTooltipActive] = useState(false);
+    const [isTooltipActive1, setIsTooltipActive1] = useState(false);
     const showTooltip = () => {
         setIsTooltipActive(true);
     };
     const hideTooltip = () => {
         setIsTooltipActive(false);
+    };
+    const showTooltip1 = () => {
+        setIsTooltipActive1(true);
+    };
+    const hideTooltip1 = () => {
+        setIsTooltipActive1(false);
     };
     const openMap = () => {
         window.open(
@@ -30,61 +38,118 @@ function StartUpPlaceToopTip({ place }) {
         // name: "(재)여성기업종합지원센터 부산센터"
         // region: "부산"
         // tel: "051-465-1001"
-        <TooltipContainer
-            ref={(element) => {
-                setPosition(element);
-            }}
-        >
-            <p id="text" onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
-                {place.name}
-            </p>
-            <ToolTip
-                active={isTooltipActive}
-                parent={posisition}
-                position="right"
-                arrow="center"
+        <>
+            <TooltipContainer
+                ref={(element) => {
+                    setPosition(element);
+                }}
             >
-                <div>
-                    <p>{place.enterprise}</p>
+                <p onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
+                    {place.name}
+                </p>
+                <ToolTip
+                    active={isTooltipActive}
+                    parent={posisition}
+                    position="right"
+                    arrow="center"
+                >
+                    <div>
+                        <p>{place.enterprise}</p>
 
-                    <p>{place.tel}</p>
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        {/*<p>{place.region}</p>*/}
+                        <p>{place.tel}</p>
                         <div
                             style={{
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'space-between',
-                                textAlign: 'center',
                             }}
                         >
-                            <p
+                            {/*<p>{place.region}</p>*/}
+                            <div
                                 style={{
-                                    cursor: 'pointer',
-                                    marginRight: 4,
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    textAlign: 'center',
                                 }}
-                                onClick={openMap}
                             >
-                                카카오지도보기
-                            </p>
-                            <RoomIcon
-                                style={{
-                                    color: 'red',
-                                    cursor: 'pointer',
-                                }}
-                                onClick={openMap}
-                            />
+                                <p
+                                    style={{
+                                        cursor: 'pointer',
+                                        marginRight: 4,
+                                    }}
+                                    onClick={openMap}
+                                >
+                                    카카오지도보기
+                                </p>
+                                <RoomIcon
+                                    style={{
+                                        color: 'red',
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={openMap}
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </ToolTip>
-        </TooltipContainer>
+                </ToolTip>
+            </TooltipContainer>
+            <AnotherTooltipContainer
+                ref={(element) => {
+                    setPosition1(element);
+                }}
+            >
+                <p onMouseEnter={showTooltip1} onMouseLeave={hideTooltip1}>
+                    {place.name}
+                </p>
+                <ToolTip
+                    active={isTooltipActive1}
+                    parent={posisition1}
+                    position="bottom"
+                    arrow="center"
+                >
+                    <div>
+                        <p>{place.enterprise}</p>
+
+                        <p>{place.tel}</p>
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                            }}
+                        >
+                            {/*<p>{place.region}</p>*/}
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                <p
+                                    style={{
+                                        cursor: 'pointer',
+                                        marginRight: 4,
+                                    }}
+                                    onClick={openMap}
+                                >
+                                    카카오지도보기
+                                </p>
+                                <RoomIcon
+                                    style={{
+                                        color: 'red',
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={openMap}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </ToolTip>
+            </AnotherTooltipContainer>
+        </>
     );
 }
 
@@ -95,5 +160,20 @@ const TooltipContainer = styled.div`
     border: 1px solid #000;
     text-align: center;
     color: #34558b;
+    display: block;
     font-weight: bold;
+    @media only screen and (max-width: 1024px) {
+        display: none;
+    }
+`;
+const AnotherTooltipContainer = styled.div`
+    width: 280px;
+    border: 1px solid #000;
+    text-align: center;
+    color: #34558b;
+    font-weight: bold;
+    display: none;
+    @media only screen and (max-width: 1024px) {
+        display: inline-block;
+    }
 `;
